c518cc6466fff46586b2c3abb6c5bac14c4429af
Caio Lourenço <lourencocaio228@gmail.com>
2022-07-04 15:13:31 -0300

    Sintase alternativa

--- Program.cs
+++ Program.cs
@@ -35,26 +35,56 @@ namespace Secao
                     new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1 }
             };
 
-            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0).ToList();
-
+            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0).ToList();
+            var r1 =
+                from p in products
+                where p.Category.Tier == 1 && p.Price <= 900.0
+                select p;
             Print("TIER 1 AND PRICE < 900:", r1);
 
-            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            // var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            var r2 =
+              from p in products
+              where p.Category.Name == "Tools"
+              select p.Name;
             Print("NAMES OF PRODUCTS FROM TOOLS", r2);
 
-            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name }); //tive q por um apelido, pois iria repetir o name duas vezes
+            //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name }); //tive q por um apelido, pois iria repetir o name duas vezes
+            var r3 =
+                from p in products
+                where p.Name[0] == 'C'
+                select new
+                {
+                    p.Name,
+                    p.Price,
+                    CategoryName = p.Category.Name,
+                };
             Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT", r3);
 
-            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            var r4 =
+                from p in products
+                where p.Category.Tier == 1
+                orderby p.Name
+                orderby p.Price
+                select p;
             Print("TIER 1 ORDER BY PRICE THEN BY NAME", r4);
 
-            var r5 = r4.Skip(2).Take(4);
+            //var r5 = r4.Skip(2).Take(4);
+            var r5 =
+                (from p in r4
+                 select p).Skip(2).Take(4);
             Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", r5);
 
-            var r6 = products.First();
+            //var r6 = products.First();
+            var r6 = (from p in products select p).First();
             Console.WriteLine("First test1: " + r6);
 
-            var r7 = products.Where(p => p.Price > 3000.0).FirstOrDefault();
+            //var r7 = products.Where(p => p.Price > 3000.0).FirstOrDefault();
+            var r7 =
+                (from p in products
+                 where p.Price > 3000.0
+                 select p).FirstOrDefault();
             Console.WriteLine("First or default test2: " + r7);// vai dar uma exeção, pode usar o FirstOrDefault, pq se ele n encontrar ngm, ele retorna null
 
             var r8 = products.Where(p => p.Id == 3).SingleOrDefault();
@@ -86,7 +116,10 @@ namespace Secao
 
             Console.WriteLine();
 
-            var r17 = products.GroupBy(p => p.Category);
+            //var r17 = products.GroupBy(p => p.Category);
+            var r17 =
+                from p in products
+                group p by p.Category;
             foreach (IGrouping<Category, Product> group in r17)
             {
                 Console.WriteLine("Category " + group.Key.Name + ":");
